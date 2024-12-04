b38ceed27196c7e430989b85bdcbaf2b41b4b00c
TrigamDev <trigambusiness@gmail.com>
2024-12-04 16:37:35 -0500

    Init

---
+++ src/client/java/dev/trigam/template/TemplateClient.java
@@ -0,0 +1,20 @@
+package dev.trigam.template;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TemplateClient implements ClientModInitializer {
+	public static final String MOD_ID = "template";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static Identifier id (String path) {
+		return Identifier.of(MOD_ID, path);
+	}
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
